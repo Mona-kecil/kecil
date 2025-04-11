7db7d854fe4b75dc48243c8bfad3e823f9c2539e
Mona-kecil <flowyourminds@gmail.com>
2025-04-11 16:04:19 +0700

    Handle getting env variable logic to one single app

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+dotenv.config()
+
+const IS_DEV: string | undefined = process.argv[2];
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const SERVER_ID = process.env.SERVER_ID || "840099499086970910";
+
+let CLIENT_ID: string;
+let TOKEN: string | undefined;
+if (IS_DEV.toUpperCase() === "DEV") {
+    TOKEN = process.env.DISCORD_DEV_TOKEN;
+    CLIENT_ID = "918108633684922398"; // Xiao Mao
+} else {
+    TOKEN = process.env.DISCORD_TOKEN;
+    CLIENT_ID = "1356142514519806143"; // Meng
+}
+
+if (!GEMINI_API_KEY) {
+    throw new Error("Gemini API key not found in environment variables.");
+}
+
+if (!TOKEN) {
+    throw new Error("Discord bot token not found in environment variables.");
+}
+
+export {
+    TOKEN,
+    IS_DEV,
+    GEMINI_API_KEY,
+    SERVER_ID,
+    CLIENT_ID
+};
